dd6295c5282c4dea58d0aa1e1e1c7a0eeb8725a7
Etienne Barassin <etiennebarassin@gmail.com>
2020-03-20 14:53:28 +0100

    commit create custom contact

--- minigarde/wp-content/themes/minigarde/functions.php
+++ minigarde/wp-content/themes/minigarde/functions.php
@@ -4,6 +4,9 @@ require get_template_directory() . '/inc/parameters.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/custom/custom-rendez-vous.php';
 
+require get_template_directory() . '/inc/custom/custom-contact.php';
+
+
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
